4e1c8d04ab04bba4a045ed0f27112ba452715d2a
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-10-06 01:21:21 +0300

    reduce db queries number.

--- blog/views.py
+++ blog/views.py
@@ -34,7 +34,7 @@ def serialize_post(post):
         'image_url': post.image.url if post.image else None,
         'published_at': post.published_at,
         'slug': post.slug,
-        'tags': [serialize_tag(tag) for tag in post.tags.all()],
+        'tags': [serialize_tag(tag) for tag in post.tags.popular()],
         'first_tag_title': post.tags.all()[0].title,
     }
 
@@ -80,7 +80,7 @@ def post_detail(request, slug):
 
     likes = post.likes.all()
 
-    related_tags = post.tags.all()
+    related_tags = post.tags.popular()
 
     serialized_post = {
         'title': post.title,
