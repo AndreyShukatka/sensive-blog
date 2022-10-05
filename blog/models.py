283b80b37c06389e994874cd610ac91847b9f630
AndreyShukatka <106096891+AndreyShukatka@users.noreply.github.com>
2022-10-06 01:01:24 +0300

    move queries into QuerySet methods.

--- blog/models.py
+++ blog/models.py
@@ -1,14 +1,27 @@
 from django.db import models
 from django.urls import reverse
 from django.contrib.auth.models import User
-from django.db.models import Count
+from django.db.models import Count, Prefetch
 
 
 class PostQuerySet(models.QuerySet):
-    def year(self, year):
-        posts_at_year = self.filter(published_at__year=year).order_by('published_at')
-        return posts_at_year
 
+    def popular(self):
+        popular_posts = self.annotate(Count('likes')).order_by('-likes__count')
+        return popular_posts
+
+    def fetch_with_comments_count(self):
+        """Возвращает список постов с числом комментариев.
+        Работает существенно быстрее, чем запрос с двумя annotate()"""
+        most_popular_posts_ids = [post.id for post in self]
+        posts_with_comments = Post.objects.filter(
+            id__in=most_popular_posts_ids).annotate(
+            comments_count=Count('comments'))
+        ids_and_comments = posts_with_comments.values_list('id', 'comments_count')
+        count_for_id = dict(ids_and_comments)
+        for post in self:
+            post.comments_count = count_for_id[post.id]
+        return self
 
 class TagQuerySet(models.QuerySet):
     def popular(self):
